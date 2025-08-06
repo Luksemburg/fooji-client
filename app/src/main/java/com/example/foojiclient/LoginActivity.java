5044e8af3c4c3746e114fc5e734f3cf973f75385
Sergey <luksemburg.77@gmail.com>
2025-08-07 01:51:33 +0300

    Login stuff in progress

---
+++ app/src/main/java/com/example/foojiclient/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.foojiclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onRegister(View view){
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLogin(View view){
+
+    }
+}
--- app/src/main/java/com/example/foojiclient/WordApiService.java
+++ app/src/main/java/com/example/foojiclient/WordApiService.java
@@ -2,6 +2,7 @@ package com.example.foojiclient;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface WordApiService {
     @GET("words/random")
     Call<List<Word>> getRandomWords(@Query("limit") int limit, @Query("mode") String mode,
                                     @Query("vocabulary") String vocabulary);
+
+    @POST("login/login")
+    Call<List<Word>> login(@Query("login") String login, @Query("password") String password);
 }
