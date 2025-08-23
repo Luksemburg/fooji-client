8046ff800f7a757c3fdcc5ecba1811d5d0a6084e
Sergey <luksemburg.77@gmail.com>
2025-08-24 01:56:36 +0300

    Registration form added

--- app/src/main/java/com/example/foojiclient/RegisterActivity.java
+++ app/src/main/java/com/example/foojiclient/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.foojiclient;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner spinner = findViewById(R.id.spinnerGender);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.spinner_items,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
