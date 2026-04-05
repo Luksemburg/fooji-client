a51870c29d9eaea1a012b119f2bfcec63ac0791c
Sergey <luksemburg.77@gmail.com>
2026-04-05 23:27:08 +0300

    Profile update in progress 2

--- app/src/main/java/com/example/foojiclient/ProfileActivity.java
+++ app/src/main/java/com/example/foojiclient/ProfileActivity.java
@@ -1,37 +1,57 @@
 package com.example.foojiclient;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText email;
+    EditText phone;
+    EditText inviteCode;
+    EditText newPassword;
+    EditText confirmNewPassword;
+    EditText oldPassword;
+    EditText location;
+    Spinner gender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         SharedPreferences prefs = getSharedPreferences("FooJiPrefs", MODE_PRIVATE);
-        String username = prefs.getString("username", " === No Data === ");
-        String email = prefs.getString("email", " === No Data === ");
-        String phone = prefs.getString("phone", " === No Data === ");
-        String location = prefs.getString("location", " === No Data === ");
-        String gender = prefs.getString("gender", null);
+        String usernameSaved = prefs.getString("username", " === No Data === ");
+        String emailSaved = prefs.getString("email", " === No Data === ");
+        String phoneSaved = prefs.getString("phone", " === No Data === ");
+        String locationSaved = prefs.getString("location", " === No Data === ");
+        String genderSaved = prefs.getString("gender", null);
 
-        EditText usernameView = findViewById(R.id.edit_username);
-        usernameView.setText(username);
-        EditText emailView = findViewById(R.id.edit_email);
-        emailView.setText(email);
-        EditText phoneView = findViewById(R.id.edit_phone);
-        phoneView.setText(phone);
-        EditText locationView = findViewById(R.id.edit_location);
-        locationView.setText(location);
+        username = findViewById(R.id.edit_username);
+        username.setText(usernameSaved);
+        email = findViewById(R.id.edit_email);
+        email.setText(emailSaved);
+        phone = findViewById(R.id.edit_phone);
+        phone.setText(phoneSaved);
+        location = findViewById(R.id.edit_location);
+        location.setText(locationSaved);
+
+        //TODO make this mechanic work
+        oldPassword = findViewById(R.id.edit_old_pwd);
+        newPassword = findViewById(R.id.edit_new_pwd);
+        confirmNewPassword = findViewById(R.id.edit_confirm_new_pwd);
 
         Spinner genderSpinner = findViewById(R.id.edit_gender);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -43,7 +63,7 @@ public class ProfileActivity extends AppCompatActivity {
         genderSpinner.setAdapter(adapter);
 
         if(gender != null){
-            setSpinnerSelectionByText(genderSpinner, gender);
+            setSpinnerSelectionByText(genderSpinner, genderSaved);
         }else{
             genderSpinner.setSelection(0);
         }
@@ -52,11 +72,86 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void onSaveProfile(View view){
+        if (validateForm()) {
+            // Submit form
+            UserDTO newUser = new UserDTO();
+            newUser.setUsername(String.valueOf(username.getText()));
+            //newUser.setEmail(String.valueOf(email.getText()));
+            newUser.setPassword(String.valueOf(newPassword.getText()));
+            newUser.setPhone(Long.parseLong(String.valueOf(phone.getText())));
+            //TODO here an error, try to register empty
+            newUser.setGender(String.valueOf(gender.getSelectedItem()));
+            newUser.setLocation(String.valueOf(location.getText()));
+            newUser.setInviteCode(Long.parseLong(String.valueOf(inviteCode.getText())));
 
+            ApiClient.getWordApiService().updateProfile(newUser).enqueue(new Callback<UserDTO>() {
+                @Override
+                public void onResponse(Call<UserDTO> call, Response<UserDTO> response) {
+                    if (response.isSuccessful()) {
+                        // handle success
+                        UserDTO createdUser = response.body();
+                        String token = response.headers().get("Authorization");
+                        Log.i("REG", "onResponse: " + response);
+
+                        SharedPreferences preferences = getSharedPreferences("FooJiPrefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("token", token);
+
+                        assert createdUser != null;
+                        editor.putString("username", createdUser.getUsername());
+                        //editor.putString("email", createdUser.getEmail());
+                        editor.putString("phone", String.valueOf(createdUser.getPhone()));
+                        editor.putString("location", createdUser.getLocation());
+                        editor.putString("gender", createdUser.getGender());
+                        editor.apply();
+
+                        Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<UserDTO> call, Throwable t) {
+                    // Handle error
+                    // TODO add more logical handling
+                    Intent intent = new Intent(ProfileActivity.this, ErrorActivity.class);
+                    if(t.getLocalizedMessage() != null) {
+                        Log.e("NETWORK ERROR", t.getLocalizedMessage());
+                        intent.putExtra("error_message", t.getLocalizedMessage());
+                    }else{
+                        Log.e("NETWORK ERROR", "UNKNOWN ERROR");
+                        intent.putExtra("error_message", "Server is not responding. Please try again later.");
+                    }
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
     public void onCancelProfile(View view){
+        finish();
+    }
 
+    private boolean validateForm() {
+        boolean valid = true;
+
+        if (username.getText().toString().trim().isEmpty()) {
+            username.setError("Required");
+            valid = false;
+        }
+
+        if (phone.getText().toString().trim().isEmpty()) {
+            phone.setError("Required");
+            valid = false;
+        }
+
+        if(!newPassword.getText().toString().trim().equals(confirmNewPassword.getText().toString().trim())){
+            confirmNewPassword.setError("Passwords do not match");
+            newPassword.setError("Passwords do not match");
+            valid = false;
+        }
+
+        return valid;
     }
 
     private void setSpinnerSelectionByText(Spinner spinner, String text) {
