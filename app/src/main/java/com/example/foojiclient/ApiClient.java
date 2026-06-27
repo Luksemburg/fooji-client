1a378427e3e6f0b0a53e67fb294e439c87ae6bb6
Sergey <luksemburg.77@gmail.com>
2026-06-28 01:17:15 +0300

    Profile update debug 2

--- app/src/main/java/com/example/foojiclient/ApiClient.java
+++ app/src/main/java/com/example/foojiclient/ApiClient.java
@@ -1,24 +1,34 @@
 package com.example.foojiclient;
 
+import android.content.Context;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://ohfak-212-8-49-35.run.pinggy-free.link"; // Use 10.0.2.2 if running on Android emulator
+    private static final String BASE_URL = "https://cjrki-212-8-49-63.free.pinggy.net"; // Use 10.0.2.2 if running on Android emulator
 
-    private static SessionManager sessionManager;
+    private static Retrofit retrofit;
+    public static void init(Context context) {
 
-    private static OkHttpClient client = new OkHttpClient.Builder()
-            .addInterceptor(new AuthInterceptor(sessionManager))
-            .build();
-    private static Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
+        SessionManager sessionManager =
+                new SessionManager(context.getApplicationContext());
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new AuthInterceptor(sessionManager))
+                .build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 
     public static WordApiService getWordApiService() {
+        if (retrofit == null) {
+            throw new IllegalStateException("ApiClient.init() was not called");
+        }
         return retrofit.create(WordApiService.class);
     }
 }
--- app/src/main/java/com/example/foojiclient/LoginActivity.java
+++ app/src/main/java/com/example/foojiclient/LoginActivity.java
@@ -28,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ApiClient.init(this);
     }
 
     public void onNewRegister(View view){
